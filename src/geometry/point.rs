921d12527cd857f2d49d4063a1492adb588c39f6
suptalentdev <suptalentdev@gmail.com>
2020-01-16 17:40:23 -0800

    Fix initial viewport setting on hidpi screens

--- src/geometry/point.rs
+++ src/geometry/point.rs
@@ -1,4 +1,3 @@
-use std::convert::TryInto;
 use std::ops;
 
 #[cfg_attr(
@@ -15,14 +14,8 @@ pub struct Point {
 impl Point {
     /// Create a new point from an x/y coordinate.
     #[inline]
-    pub fn new<T>(x: T, y: T) -> Point
-    where
-        T: TryInto<i32>,
-    {
-        Point {
-            x: x.try_into().ok().unwrap(),
-            y: y.try_into().ok().unwrap(),
-        }
+    pub fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
     }
 
     // Create a zero point
@@ -33,34 +26,8 @@ impl Point {
 
     #[inline]
     // Create a point from a tuple of two i32s
-    pub fn from_tuple<T>(t: (T, T)) -> Self
-    where
-        T: TryInto<i32>,
-    {
-        Point::new(t.0, t.1)
-    }
-
-    #[inline]
-    // Helper for map index conversion
-    pub fn to_index<T>(&self, width: T) -> usize
-    where
-        T: TryInto<usize>,
-    {
-        let x: usize = self.x.try_into().ok().unwrap();
-        let y: usize = self.y.try_into().ok().unwrap();
-        let w: usize = width.try_into().ok().unwrap();
-        (y * w) + x
-    }
-
-    pub fn to_tuple(&self) -> (i32, i32) {
-        (self.x, self.y)
-    }
-
-    pub fn to_unsigned_tuple(&self) -> (usize, usize) {
-        (
-            self.x.try_into().ok().unwrap(),
-            self.y.try_into().ok().unwrap(),
-        )
+    pub fn from_tuple(t: (i32, i32)) -> Self {
+        Point { x: t.0, y: t.1 }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![warn(clippy::pedantic)]
 #[macro_use]
 extern crate lazy_static;
 mod codepage437;
@@ -17,11 +16,10 @@ mod simple_console;
 mod sparse_console;
 pub mod textblock;
 pub use hal::*;
-mod algorithm2d;
-mod algorithm3d;
-mod basemap;
 #[cfg(feature = "parsing")]
 mod parsing;
+mod rect;
+pub use rect::Rect;
 
 pub use self::codepage437::{string_to_cp437, to_char, to_cp437};
 pub use self::color::*;
@@ -30,7 +28,7 @@ pub use self::fastnoise::*;
 pub use self::fieldofview::{field_of_view, field_of_view_set};
 pub use self::font::Font;
 pub use self::geometry::{
-    line2d, project_angle, Bresenham, BresenhamCircle, DistanceAlg, LineAlg, Point, Point3, Rect,
+    line2d, project_angle, Bresenham, BresenhamCircle, DistanceAlg, LineAlg, Point, Point3,
     VectorLine,
 };
 pub use self::pathfinding::astar::{a_star_search, NavigationPath};
@@ -40,16 +38,9 @@ pub use self::rltk::{letter_to_option, main_loop, Rltk};
 pub use self::simple_console::SimpleConsole;
 pub use self::sparse_console::SparseConsole;
 pub use self::textblock::{TextBlock, TextBuilder};
-pub use algorithm2d::Algorithm2D;
-pub use algorithm3d::Algorithm3D;
-pub use basemap::BaseMap;
 #[cfg(feature = "parsing")]
 pub use parsing::{parse_dice_string, DiceParseError, DiceType};
 pub mod embedding;
-
-pub mod prelude {
-    pub use crate::*;
-}
 
 #[macro_export]
 macro_rules! add_wasm_support {
@@ -104,4 +95,43 @@ pub use hal::VirtualKeyCode;
 /// Implement this trait on your state struct, so the engine knows what to call on each tick.
 pub trait GameState: 'static {
     fn tick(&mut self, ctx: &mut Rltk);
+}
+
+/// Implement this trait to support path-finding functions.
+pub trait BaseMap {
+    /// True is you can see through the tile, false otherwise.
+    fn is_opaque(&self, idx: i32) -> bool;
+
+    /// Return a vector of tile indices to which one can path from the idx.
+    /// These do NOT have to be contiguous - if you want to support teleport pads, that's awesome.
+    fn get_available_exits(&self, idx: i32) -> Vec<(i32, f32)>;
+
+    /// Return the distance you would like to use for path-finding. Generally, Pythagoras distance (implemented in geometry)
+    /// is fine, but you might use Manhattan or any other heuristic that fits your problem.
+    fn get_pathing_distance(&self, idx1: i32, idx2: i32) -> f32;
+}
+
+/// Implement these for handling conversion to/from 2D coordinates (they are separate, because you might
+/// want Dwarf Fortress style 3D!)
+pub trait Algorithm2D: BaseMap {
+    /// Convert a Point (x/y) to an array index.
+    fn point2d_to_index(&self, pt: Point) -> i32;
+
+    /// Convert an array index to a point.
+    fn index_to_point2d(&self, idx: i32) -> Point;
+
+    // Optional - check that an x/y coordinate is within the map bounds
+    fn in_bounds(&self, _pos: Point) -> bool {
+        true
+    }
+}
+
+/// Implement these for handling conversion to/from 2D coordinates (they are separate, because you might
+/// want Dwarf Fortress style 3D!)
+pub trait Algorithm3D: BaseMap {
+    /// Convert a Point (x/y) to an array index.
+    fn point3d_to_index(&self, pt: Point3) -> i32;
+
+    /// Convert an array index to a point.
+    fn index_to_point3d(&self, idx: i32) -> Point3;
 }
